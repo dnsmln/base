531790e2e9b0ce4e9eabd47bde504c47b4a26ca7
Denis <159943467+dnsmln@users.noreply.github.com>
2025-08-26 20:51:43 -0400

    head title fix

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const SITE_CONFIG = {
     siteTitle: "Words, colors, and shapes",
-    description: "Personal website of Denis Moulin",
+    description: "Home of Denis Moulin",
     ogImage: "https://denismoulin.com/og-generic.jpg"
 };
